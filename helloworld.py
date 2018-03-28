2430c641251f348d9f11e591131102d1abf60ab5
alternat0 <alternat0@gmail.com>
2018-03-28 08:55:01 +0700

    First file commit

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+# dictionary (maps): values with unique keys
+# tidak bisa di-join
+# dictionary dalam python: unordered
+
+daftar_superhero = {'Barol': 'Johanes Pinatiti', 'Superman': 'Clark Kent', 'Batman': 'Bruce Wayne'}
+print(daftar_superhero)
+
+daftar_superhero['Spiderman'] = 'Peter Parker'
+print(daftar_superhero)
+
+print(daftar_superhero['Superman'])
+
+daftar_superhero['Barol'] = 'Johanes'
+print(daftar_superhero)
+
+del daftar_superhero['Barol']
+print(daftar_superhero)
+
+print(len(daftar_superhero))
+print(daftar_superhero.get('Batman'))
+print(daftar_superhero.keys())
+print(daftar_superhero.values())
+
+Masukkan singkatan kota: jkt
+Kota yang anda maksud: Jakarta
+
+gunakan Dictionary
