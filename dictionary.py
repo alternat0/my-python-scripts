575e646f9615c3143cdb8d3764e07eb11419e79f
alternat0 <danny@rayasem.com>
2018-04-02 11:46:38 +0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,5 +3,8 @@
 # dictionary dalam python: unordered
 # perubahan di git bash(penambahan command)
 
+#Nama : Dika Akmaliza
+#NIM : 20917001
 
-
+daftar_superhero = {'Barol': 'Johanes Pinatiti', 'Superman': 'Clark Kent', 'Batman': 'Bruce Wayne'}
+print(daftar_superhero)
