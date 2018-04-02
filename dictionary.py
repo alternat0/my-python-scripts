eefbc572ed1ec2ab042880dccdea7083340e17b6
DikaAkmaliza18 <akmalizadika@users.noreply.github.com>
2018-04-01 21:38:23 -0700

    Comment

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,9 @@
 # tidak bisa di-join
 # dictionary dalam python: unordered
 
+#Nama : Dika Akmaliza
+#NIM : 20917001
+
 daftar_superhero = {'Barol': 'Johanes Pinatiti', 'Superman': 'Clark Kent', 'Batman': 'Bruce Wayne'}
 print(daftar_superhero)
 
