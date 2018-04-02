b547945ed3dea75b154c665ed33705862ba2b9ba
Gabrielunpris <gabrielbrandt07@gmail.com>
2018-04-02 12:25:27 +0800

    rubah

--- dictionary.py
+++ dictionary.py
@@ -2,22 +2,5 @@
 # tidak bisa di-join
 # dictionary dalam python: unordered
 
-daftar_superhero = {'Barol': 'Johanes Pinatiti', 'Superman': 'Clark Kent', 'Batman': 'Bruce Wayne'}
-print(daftar_superhero)
 
-daftar_superhero['Spiderman'] = 'Peter Parker'
-print(daftar_superhero)
-
-print(daftar_superhero['Superman'])
-
-daftar_superhero['Barol'] = 'Johanes'
-print(daftar_superhero)
-
-del daftar_superhero['Barol']
-print(daftar_superhero)
-
-print(len(daftar_superhero))
-print(daftar_superhero.get('Batman'))
-print(daftar_superhero.keys())
-print(daftar_superhero.values())
 
